b61de39adb953d816905d07e15f1739da973b221
Matthew <kjcxz9f937@yandex.ru>
2024-02-11 19:47:14 +0300

    Внедрил программу Егора в Game и сделал рестарт игры, теперь группы спрайтов - атрибуты класса Game.

--- game.py
+++ game.py
@@ -3,19 +3,13 @@ from borders import HorizontalBorder, VerticalBorder, RoundedBorder
 from coin import Coin
 from boost import Boost
 from ghosts import Blinky, Pinky, Inky, Clyde
+import sys
 from packman import PackMan
 from fruits import Fruits
 from random import choice
 from board import boards
 from copy import deepcopy
 from load_image_function import load_image
-
-# Группы спрайтов для отрисовки и взаимодействий
-packman_sprite = pygame.sprite.Group()
-ghosts_sprites = pygame.sprite.Group()
-coins_sprites = pygame.sprite.Group()
-boosts_sprites = pygame.sprite.Group()
-fruits_sprites = pygame.sprite.Group()
 
 
 # Класс игры
@@ -30,12 +24,19 @@ class Game:
         self.height = 31
         self.board = deepcopy(boards)
         self.screen = screen
-        self.packman = PackMan(packman_sprite)
+        # Группы спрайтов для отрисовки и взаимодействий
+        self.packman_sprite = pygame.sprite.Group()
+        self.ghosts_sprites = pygame.sprite.Group()
+        self.boosts_sprites = pygame.sprite.Group()
+        self.coins_sprites = pygame.sprite.Group()
+        self.fruits_sprites = pygame.sprite.Group()
+        self.packman = PackMan(self.packman_sprite)
         self.packman.parent_game = self
-        self.blinky = Blinky(ghosts_sprites)
-        self.pinky = Pinky(ghosts_sprites)
-        self.inky = Inky(ghosts_sprites)
-        self.clyde = Clyde(ghosts_sprites)
+        self.blinky = Blinky(self.ghosts_sprites)
+        self.pinky = Pinky(self.ghosts_sprites)
+        self.inky = Inky(self.ghosts_sprites)
+        self.clyde = Clyde(self.ghosts_sprites)
+        self.restart_flag = False
         self.points = 0  # Очки, заработанные пакманом
         self.lives = 3  # Жизни пакмана
         self.fruit = None  # Фрукт в игре(None, когда нету фрукта)
@@ -60,9 +61,9 @@ class Game:
         for i in range(self.width):
             for j in range(self.height):
                 if self.board[j][i] == 1:
-                    Coin(i, j, coins_sprites)
+                    Coin(i, j, self.coins_sprites)
                 if self.board[j][i] == 2:
-                    Boost(i, j, boosts_sprites)
+                    Boost(i, j, self.boosts_sprites)
 
         with open('high_score.txt', 'r') as file:
             self.high_score = int(file.read().strip())
@@ -97,7 +98,8 @@ class Game:
             self.screen.fill((0, 0, 0))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    pygame.quit()
+                    sys.exit()
                 if event.type == dots_update_event:
                     if dots == 3:
                         dots = 0
@@ -113,14 +115,35 @@ class Game:
             pygame.display.flip()
 
     def game_over(self, win):
-        if win:
-            pass
-        else:
-            pass
         if self.points > self.high_score:
             with open('high_score.txt', 'r+') as file:
                 file.truncate(0)
                 file.write(str(self.points))
+
+        running = True
+        while running:
+            self.screen.fill((0, 0, 0))
+
+            # Рисуем текст диалогового окна
+            if win:
+                self.show_text(110, 'YOU WIN!', (40, 60))
+            else:
+                self.show_text(100, 'OOPS...', (100, 60))
+            self.show_text(90, "GAME OVER", (35, 250))
+            self.show_text(70, "1. RESTART", (90, 400))
+            self.show_text(70, "2. FINISH", (90, 550))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            if pygame.key.get_pressed()[pygame.K_1]:
+                self.restart_flag = True
+                running = False
+            elif pygame.key.get_pressed()[pygame.K_2]:
+                pygame.quit()
+                sys.exit()
+            pygame.display.flip()
 
     def update_packman_direction(self):
         # Изменение направления движения пакмана
@@ -134,14 +157,14 @@ class Game:
             self.packman.change_direction('RIGHT')
 
     def render(self):
+        self.show_information()
         # Отрисовка спрайтов
-        global ghosts_sprites
-        coins_sprites.draw(self.screen)
-        boosts_sprites.draw(self.screen)
+        self.coins_sprites.draw(self.screen)
+        self.boosts_sprites.draw(self.screen)
         if self.fruit is not None:
-            fruits_sprites.draw(self.screen)
-        ghosts_sprites.draw(self.screen)
-        packman_sprite.draw(self.screen)
+            self.fruits_sprites.draw(self.screen)
+        self.ghosts_sprites.draw(self.screen)
+        self.packman_sprite.draw(self.screen)
         for i in range(self.width):
             for j in range(self.height):
                 if 9 > self.board[j][i] > 4:
@@ -153,7 +176,7 @@ class Game:
         # Условие появления фрукта
         if (self.fruit_type < 3 and self.points - self.points_received_from_fruits > self.fruit_levels[
             self.fruit_type]):
-            self.fruit = Fruits(choice([14, 13]), 17, self.fruit_type, fruits_sprites)
+            self.fruit = Fruits(choice([14, 13]), 17, self.fruit_type, self.fruits_sprites)
             self.fruit_type += 1
             pygame.time.set_timer(self.FRUIT_EXISTENCE_TIMER, 9600)
         # Условие возрождения призрака
@@ -161,11 +184,11 @@ class Game:
             if ghost.mode == 'RETURNING_HOME' and ghost.get_position() == ghost.respawn_point:
                 ghost.mode = 'HARASSMENT'
         # Столкновение пакмана с манеткой
-        if pygame.sprite.spritecollide(self.packman, coins_sprites, True):
+        if pygame.sprite.spritecollide(self.packman, self.coins_sprites, True):
             self.points += 10
             self.items_count -= 1
         # Столкновение пакмана с энерджайзером
-        if pygame.sprite.spritecollide(self.packman, boosts_sprites, True):
+        if pygame.sprite.spritecollide(self.packman, self.boosts_sprites, True):
             self.points += 50
             self.items_count -= 1
             self.packman.boost_mode = True
@@ -173,27 +196,27 @@ class Game:
                 ghost.mode = 'FRIGHT'
             pygame.time.set_timer(Game.PACKMAN_MOVE_EVENT, 70)
         # Столкновение пакмана с фруктом
-        if pygame.sprite.spritecollideany(self.packman, fruits_sprites):
+        if pygame.sprite.spritecollideany(self.packman, self.fruits_sprites):
             self.points += self.fruit.points
             self.points_received_from_fruits += self.fruit.points
-            fruits_sprites.empty()
+            self.fruits_sprites.empty()
             self.fruit = None
             pygame.time.set_timer(self.FRUIT_EXISTENCE_TIMER, 0)
         # Столкновение пакмана с призраком
-        if pygame.sprite.spritecollideany(self.packman, ghosts_sprites):
-            if pygame.sprite.spritecollideany(self.packman, ghosts_sprites).mode != 'RETURNING_HOME':
+        if pygame.sprite.spritecollideany(self.packman, self.ghosts_sprites):
+            if pygame.sprite.spritecollideany(self.packman, self.ghosts_sprites).mode != 'RETURNING_HOME':
                 if self.packman.boost_mode:
-                    pygame.sprite.spritecollideany(self.packman, ghosts_sprites).mode = 'RETURNING_HOME'
+                    pygame.sprite.spritecollideany(self.packman, self.ghosts_sprites).mode = 'RETURNING_HOME'
                     self.eaten_ghosts += 1
                     self.points += 2 ** self.eaten_ghosts * 100
                 else:
                     self.lives -= 1
                     if self.lives:
-                        ghosts_sprites.empty()
-                        self.blinky = Blinky(ghosts_sprites)
-                        self.pinky = Pinky(ghosts_sprites)
-                        self.inky = Inky(ghosts_sprites)
-                        self.clyde = Clyde(ghosts_sprites)
+                        self.ghosts_sprites.empty()
+                        self.blinky = Blinky(self.ghosts_sprites)
+                        self.pinky = Pinky(self.ghosts_sprites)
+                        self.inky = Inky(self.ghosts_sprites)
+                        self.clyde = Clyde(self.ghosts_sprites)
                         self.ghosts = (self.blinky, self.pinky, self.inky, self.clyde)
                         for ghost in self.ghosts:
                             ghost.parent_game = self
@@ -203,7 +226,6 @@ class Game:
         # Если закончаться монетки и энерджайзеры - конец игры
         if not self.items_count:
             self.game_over(True)
-        self.show_information()
 
     def update_ghosts(self):
         # Обновление призраков
@@ -214,5 +236,3 @@ class Game:
         # Обновление пакмана
         self.update_packman_direction()
         self.packman.update()
-
-
